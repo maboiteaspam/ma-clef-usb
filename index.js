c5c0a99ef46c10c585a3a9fb825d7c191cd4c120
clement <maboiteaspam@wanadoo.fr>
2014-11-29 00:27:23 +0800

    update server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var fs = require('fs-extra');
 var pathExtra = require('path-extra');
+var glob = require("glob");
+var mime = require('mime');
 var express = require('express');
 var http = require('http');
 var busboy = require('connect-busboy');
@@ -11,77 +13,151 @@ var pkg = require(__dirname+'/package.json');
 
 var maClefUsbPath = pathExtra.join(pathExtra.homedir(), 'ma-clef-usb');
 
-var maClefUsb = (function(storagePath){
+var maClefUsb = (function(){
 
-  if( fs.existsSync(storagePath) == false ){
-    fs.mkdirSync(storagePath);
-  }
+  var storagePath = '';
 
   var api = {
-    list: function( path, done ){
-      path = path || '';
-
-      done([
-        {
-          fileName: '',
-          path: '',
-          size: '',
-          mtime: ''
-        }
-      ]);
-    },
-    write: function(path, fileName, file, done){
-      var fstream = fs.createWriteStream( storagePath + path + fileName );
-      file.pipe(fstream);
-      fstream.on('close', function () {
-        done({
-          fileName: '',
-          path: '',
-          size: '',
-          mtime: ''
+    write: function(storePath, fileName, file, done){
+      var storeFilePath = pathExtra.normalize(storePath);
+      storeFilePath = pathExtra.join(storeFilePath, fileName);
+      var rstoreFilePath = pathExtra.resolve(storagePath, storeFilePath);
+      if( rstoreFilePath.match(new RegExp("^"+storagePath)) ){
+        var fstream = fs.createWriteStream( rstoreFilePath );
+        file.pipe(fstream);
+        fstream.on('close', function () {
+          api.readdir(storePath, done);
         });
-      });
+      }else{
+        done('not-found');
+      }
     },
     rename: function(oldPath, newPath, done){
-      done({
-        oldPath: oldPath,
-        newPath: newPath,
-        fileName: '',
-        path: '',
-        size: '',
-        mtime: ''
-      });
+      newPath = pathExtra.normalize(newPath);
+      oldPath = pathExtra.normalize(oldPath);
+      var roldPath = pathExtra.resolve(storagePath, oldPath);
+      var rnewPath = pathExtra.resolve(storagePath, newPath);
+      if( roldPath.match(new RegExp("^"+storagePath))
+        && rnewPath.match(new RegExp("^"+storagePath)) ){
+        fs.rename(roldPath, rnewPath, done);
+      }else{
+        done('not-found');
+      }
     },
-    read: function(filePath, done){
-      done({
-        filePath: filePath,
-        fileName: '',
-        path: '',
-        size: '',
-        mtime: ''
-      });
+    readfile: function(dirPath, done){
+      dirPath = pathExtra.normalize(dirPath);
+      var rPath = pathExtra.resolve(storagePath, dirPath);
+      if( rPath.match(new RegExp("^"+storagePath)) ){
+        glob("/*", {cwd:rPath}, function (er, files) {
+          var items = [];
+          files.forEach(function(file){
+            var filePath = pathExtra.join(filePath, file);
+            var stat = fs.lstatSync(filePath);
+            items.push({
+              type: stat.isFile()?'file':'folder',
+              path: filePath,
+              name: file,
+              ext: pathExtra.extname(file),
+              size: stat.size,
+              mtime: stat.mtime
+            })
+          });
+          done(items);
+        });
+      }else{
+        done('not-found');
+      }
+    },
+    readdir: function(dirPath, done){
+      dirPath = pathExtra.normalize(dirPath);
+      var rPath = pathExtra.resolve(storagePath, dirPath);
+      if( rPath.match(new RegExp("^"+storagePath)) ){
+        glob("/*", {cwd:rPath}, function (er, files) {
+          var items = [];
+          files.forEach(function(file){
+            var filePath = pathExtra.join(filePath, file);
+            var stat = fs.lstatSync(filePath);
+            items.push({
+              type: stat.isFile()?'file':'folder',
+              path: filePath,
+              name: file,
+              ext: pathExtra.extname(file),
+              size: stat.size,
+              mtime: stat.mtime
+            })
+          });
+          done(items);
+        });
+      }else{
+        done('not-found');
+      }
     },
     remove: function(filePath, done){
-      done(true);
+      filePath = pathExtra.normalize(filePath);
+      var rPath = pathExtra.resolve(storagePath, filePath);
+      if( rPath.match(new RegExp("^"+filePath)) ){
+        fs.remove( rPath, done)
+      }else{
+        done('not-found');
+      }
+    },
+    changeHome: function(newPath, done){
+      if( fs.existsSync(storagePath) == false ){
+        done('not-found')
+      } else{
+        storagePath = path.resolve(newPath);
+        done(true)
+      }
     }
   };
   return api;
-})(maClefUsbPath);
+})();
 
+
+if( fs.existsSync(maClefUsbPath) == false ){
+  fs.mkdirSync(maClefUsbPath);
+}
+maClefUsb.changeHome(maClefUsbPath);
 
 var start = function(options, done) {
   var app = express();
   app.use(busboy());
-  app.get('/list', function(req, res){
+  app.get('/change-home', function(req, res){
+    var newPath = req.body.newPath;
+    maClefUsb.changeHome(newPath,function(ok){
+      res.status(ok?200:404).send()
+    });
+  });
+  app.get('/readdir', function(req, res){
     var filePath = req.body.filePath;
-    maClefUsb.list(filePath,function(list){
+    maClefUsb.readdir(filePath,function(list){
       res.send( list );
+    });
+  });
+  app.get('/readfile', function(req, res){
+    var filePath = req.body.filePath;
+    maClefUsb.readfile(filePath,function(err, stream){
+      var type = mime.lookup(filePath);
+      if (!res.getHeader('content-type')) {
+        var charset = mime.charsets.lookup(type);
+        res.setHeader('Content-Type', type + (charset ? '; charset=' + charset : ''));
+      }
+      stream.pipe(res);
+    });
+  });
+  app.get('/rename', function(req, res){
+    var oldPath = req.body.oldPath;
+    var newPath = req.body.newPath;
+    maClefUsb.rename(oldPath, newPath,function(success){
+      var ok = success == 'not-found';
+      res.status(ok?200:404).send()
     });
   });
   app.get('/remove', function(req, res){
     var filePath = req.body.filePath;
     maClefUsb.remove(filePath,function(success){
-      res.send( success );
+      var ok = success == 'not-found';
+      res.status(ok?200:404).send()
     });
   });
   app.post('/add', function(req, res){
