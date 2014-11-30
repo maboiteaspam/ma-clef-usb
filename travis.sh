2abbd6841c2a558087f039f0c7a11c4fd832f7b2
clement <maboiteaspam@wanadoo.fr>
2014-11-30 18:54:58 +0800

    update Tests

--- travis.sh
+++ travis.sh
@@ -2,4 +2,10 @@
 set -e
 
 npm test
+
+npm i maboiteaspam/cozy-light -g
+cozy-light add-plugin maboiteaspam/cozy-homepage
+cozy-light install maboiteaspam/cozy-dashboard
+cozy-light install maboiteaspam/ma-clef-usb
+cozy-light start &
 npm run integration
