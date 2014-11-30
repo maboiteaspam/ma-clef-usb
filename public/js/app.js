f597336d32817b1665f0a7373332f9653ce1b048
clement <maboiteaspam@wanadoo.fr>
2014-11-30 14:56:49 +0800

    fixed preview picture size

--- public/js/app.js
+++ public/js/app.js
@@ -13,5 +13,26 @@ angular
         'angularMoment',
         'angular-underscore'
     ])
+  .directive('maxHeight', function() {
+
+    function link(scope, element, attrs) {
+      element.on('$destroy', function() {
+      });
+      element.one('load', function() {
+        var w = angular.element(window);
+        if( element.height() > w.height() ){
+          element.css("max-height", w.height()-100 );
+        }
+        if( element.width() > w.width() ){
+          element.css("max-width", w.width()-100 );
+        }
+      });
+    }
+
+    return {
+      restrict: 'A',
+      link: link
+    };
+  })
 
 ;
