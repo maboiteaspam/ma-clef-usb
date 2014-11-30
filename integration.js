fd6b76e9c4ae904797aff63a768253133b0e6ae7
clement <maboiteaspam@wanadoo.fr>
2014-11-30 19:03:53 +0800

    update Tests

--- integration.js
+++ integration.js
@@ -5,5 +5,22 @@ var express = require('express');
 var request = require('request');
 var assert = require('assert');
 
-var maClefUsb = require('./ma-clef-usb');
-var controllers = require('./controllers');
+
+describe('Controllers', function () {
+  before(function(){
+  });
+  describe('readdir', function () {
+    it('should answer 200', function (done) {
+      request.get('http://localhost:19104/ma-clef-usb',
+        function(error, response, body) {
+          assert.equal(error,null,'error must be null')
+          assert.equal(response.statusCode,200,'must respond 200')
+          assert.ok(body.match(/html/),'must respond html')
+          done();
+        });
+    });
+  });
+
+
+});
+
